4b370624bef0bef3e2c0459c9c0e6e942236765f
jeenapv <jeenapv1992@gmail.com>
2016-08-24 11:23:23 +0530

    LInk added in OrganizationTransferHistory

--- src/view/OrganizationTransferHistory.java
+++ src/view/OrganizationTransferHistory.java
@@ -181,6 +181,11 @@ public class OrganizationTransferHistory extends javax.swing.JFrame {
         jLabel6.setText("Priority");
 
         jButton1.setText("HOME");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -243,6 +248,13 @@ public class OrganizationTransferHistory extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        this.dispose();
+        OrganizationHome organizationHome=new OrganizationHome();
+        organizationHome.setVisible(true);
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
