7cc61c004a00a9a4bb18e96c4f7cefda2be37a1a
krishnakumarvs <krishh_mea@yahoo.in>
2016-09-11 22:48:38 +0530

    Refresh added in home. Inter organization data member reject file request completed.

--- src/view/DataMemberHome.java
+++ src/view/DataMemberHome.java
@@ -6,6 +6,7 @@
 package view;
 
 import Db.Dbcon;
+import General.Configuration;
 import java.sql.ResultSet;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -17,6 +18,8 @@ import javax.swing.table.DefaultTableModel;
  * @author Jithinpv
  */
 public class DataMemberHome extends javax.swing.JFrame {
+    
+    private static String title = "";
 
     /**
      * Creates new form DataMemberHome
@@ -26,22 +29,36 @@ public class DataMemberHome extends javax.swing.JFrame {
         this.setLocationRelativeTo(null);
         loadFileRequests();
         setTitle(title);
+        loadIcons();
     }
-    private static String title = "";
-
+    
     public DataMemberHome(String userName) {
         initComponents();
         this.setLocationRelativeTo(null);
         loadFileRequests();
         title = userName + " - Home";
         setTitle(title);
+        loadIcons();
     }
-
+    
+    private void loadIcons() {
+        Configuration.setIconOnLabel("refresh.png", refresh_label);
+    }
+    
+    private void clearTable() throws Exception {
+        DefaultTableModel dm = (DefaultTableModel) request_table.getModel();
+        int rowCount = dm.getRowCount();
+        for (int i = rowCount - 1; i >= 0; i--) {
+            dm.removeRow(i);
+        }
+    }
+    
     private void loadFileRequests() {
         try {
+            clearTable();
             approve_button.setEnabled(false);
             reject_button.setEnabled(false);
-
+            
             Dbcon dbcon = new Dbcon();
             DefaultTableModel model = (DefaultTableModel) request_table.getModel();
             String str = "SELECT filereq.request_id,filereq.requested_date, filereq.request_priority, filereq.requested_data_member ,filereq.status,filereq.is_inter_company_file_request, "
@@ -59,7 +76,7 @@ public class DataMemberHome extends javax.swing.JFrame {
                     + " encrptlog.encryption_id = filereq.encryption_id AND"
                     + " filereq.file_owner_data_member =" + DataMemberLogin.logged_in_user_id;
             ResultSet rs = dbcon.select(str);
-
+            
             String arr[] = new String[10];
             while (rs.next()) {
                 String request_id = rs.getString("request_id");
@@ -75,9 +92,9 @@ public class DataMemberHome extends javax.swing.JFrame {
                 } else if (request_priority.equals("2")) {
                     request_priority = "High";
                 }
-
+                
                 String status = rs.getString("status").trim();
-
+                
                 if (status.equals("0")) {
                     status = "Rejected";
                 } else if (status.equals("1")) {
@@ -85,9 +102,9 @@ public class DataMemberHome extends javax.swing.JFrame {
                 } else if (status.equals("2")) {
                     status = "Pending";
                 }
-
+                
                 String is_inter_company_file_request = rs.getString("is_inter_company_file_request");
-
+                
                 arr[0] = request_id;
                 arr[1] = dataMemName;
                 arr[2] = org_name;
@@ -96,18 +113,18 @@ public class DataMemberHome extends javax.swing.JFrame {
                 arr[5] = request_priority;
                 arr[6] = status;
                 arr[7] = is_inter_company_file_request;
-
+                
                 model.addRow(arr);
             }
-
-
+            
+            
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
+    
     private String getFormatedDate(String dateString, String format) {
-
+        
         try {
             long dateMilli = Long.parseLong(dateString);
             Date date = new Date(dateMilli);
@@ -140,6 +157,7 @@ public class DataMemberHome extends javax.swing.JFrame {
         request_table = new javax.swing.JTable();
         approve_button = new javax.swing.JButton();
         reject_button = new javax.swing.JButton();
+        refresh_label = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         addWindowListener(new java.awt.event.WindowAdapter() {
@@ -237,6 +255,14 @@ public class DataMemberHome extends javax.swing.JFrame {
             }
         });
 
+        refresh_label.setText(" ");
+        refresh_label.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        refresh_label.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                refresh_labelMouseClicked(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -244,7 +270,7 @@ public class DataMemberHome extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addGroup(layout.createSequentialGroup()
                                 .addGap(18, 18, 18)
                                 .addComponent(jButton2)
@@ -255,10 +281,12 @@ public class DataMemberHome extends javax.swing.JFrame {
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addComponent(jButton5)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED))
-                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                                .addContainerGap()
-                                .addComponent(jLabel1)
-                                .addGap(218, 218, 218)))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(32, 32, 32)
+                                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 156, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(refresh_label, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(36, 36, 36)))
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jButton1)
                             .addComponent(jButton6))
@@ -279,7 +307,8 @@ public class DataMemberHome extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton1)
-                    .addComponent(jLabel1))
+                    .addComponent(jLabel1)
+                    .addComponent(refresh_label, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(35, 35, 35)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton2)
@@ -293,7 +322,7 @@ public class DataMemberHome extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(approve_button)
                     .addComponent(reject_button))
-                .addContainerGap(30, Short.MAX_VALUE))
+                .addContainerGap(12, Short.MAX_VALUE))
         );
 
         pack();
@@ -305,42 +334,42 @@ public class DataMemberHome extends javax.swing.JFrame {
         ViewAllFiles viewAllFiles = new ViewAllFiles();
         viewAllFiles.setVisible(true);
     }//GEN-LAST:event_jButton3ActionPerformed
-
+    
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
         // TODO add your handling code here:
         this.dispose();
         ChooseFile chooseFile = new ChooseFile();
         chooseFile.setVisible(true);
     }//GEN-LAST:event_jButton5ActionPerformed
-
+    
     private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
         // TODO add your handling code here:
         this.dispose();
         EncryptionHistory encryptionHistory = new EncryptionHistory();
         encryptionHistory.setVisible(true);
     }//GEN-LAST:event_jButton6ActionPerformed
-
+    
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
         this.dispose();
         DataMemberTransferHistory dataMemberTransferHistory = new DataMemberTransferHistory();
         dataMemberTransferHistory.setVisible(true);
     }//GEN-LAST:event_jButton2ActionPerformed
-
+    
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
         // TODO add your handling code here:
         this.dispose();
         ViewRequestedFileStatus viewRequestedFileStatus = new ViewRequestedFileStatus();
         viewRequestedFileStatus.setVisible(true);
     }//GEN-LAST:event_jButton4ActionPerformed
-
+    
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
         this.dispose();
         MainLogin mainlogin = new MainLogin();
         mainlogin.setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
-
+    
     private void approve_buttonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_approve_buttonActionPerformed
         // TODO add your handling code here:
 
@@ -366,20 +395,20 @@ public class DataMemberHome extends javax.swing.JFrame {
             // outside company request
             JOptionPane.showMessageDialog(rootPane, "Different company");
         }
-
-
+        
+        
     }//GEN-LAST:event_approve_buttonActionPerformed
-
+    
     private void formWindowOpened(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowOpened
         // TODO add your handling code here:
     }//GEN-LAST:event_formWindowOpened
-
+    
     private void reject_buttonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_reject_buttonActionPerformed
         // TODO add your handling code here:
         String request_id = request_table.getValueAt(request_table.getSelectedRow(), 0).toString();
         String status = request_table.getValueAt(request_table.getSelectedRow(), 6).toString();
         String is_inter_company_file_request = request_table.getValueAt(request_table.getSelectedRow(), 7).toString();
-
+        
         if (is_inter_company_file_request.equals("0")) {
             // iside company request
             if (status.toLowerCase().equals("approved") || status.toLowerCase().equals("rejected")) {
@@ -396,15 +425,24 @@ public class DataMemberHome extends javax.swing.JFrame {
             }
         } else {
             // outside company request
+            Dbcon dbcon = new Dbcon();
+            int updated = dbcon.update("update tbl_file_request set approve_reject_date='" + System.currentTimeMillis() + "' , status='0' , remark='Data owner rejeted' where request_id='" + request_id + "'");
+            if (updated > 0) {
+                request_table.setValueAt("Rejected", request_table.getSelectedRow(), 6);
+                reject_button.setEnabled(false);
+                approve_button.setEnabled(false);
+            } else {
+                JOptionPane.showMessageDialog(rootPane, "Could not reject now, please try again later");
+            }
         }
     }//GEN-LAST:event_reject_buttonActionPerformed
-
+    
 private void request_tableMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_request_tableMouseClicked
-
+    
     String request_id = request_table.getValueAt(request_table.getSelectedRow(), 0).toString();
     String status = request_table.getValueAt(request_table.getSelectedRow(), 6).toString();
     String is_inter_company_file_request = request_table.getValueAt(request_table.getSelectedRow(), 7).toString();
-
+    
     if (status.toLowerCase().equals("approved") || status.toLowerCase().equals("rejected")) {
         approve_button.setEnabled(false);
         reject_button.setEnabled(false);
@@ -416,6 +454,13 @@ private void request_tableMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIR
 
     // TODO add your handling code here:
 }//GEN-LAST:event_request_tableMouseClicked
+    
+private void refresh_labelMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_refresh_labelMouseClicked
+    
+    loadFileRequests();
+    JOptionPane.showMessageDialog(rootPane, "Refresh completed");
+    // TODO add your handling code here:
+}//GEN-LAST:event_refresh_labelMouseClicked
 
     /**
      * @param args the command line arguments
@@ -446,7 +491,7 @@ private void request_tableMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIR
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
-
+            
             public void run() {
                 new DataMemberHome().setVisible(true);
             }
@@ -462,6 +507,7 @@ private void request_tableMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIR
     private javax.swing.JButton jButton6;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel refresh_label;
     private javax.swing.JButton reject_button;
     private javax.swing.JTable request_table;
     // End of variables declaration//GEN-END:variables
